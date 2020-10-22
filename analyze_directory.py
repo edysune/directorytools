45752e480965e8e37f918f0a6238273717b4b20b
Edison Earnest <dayxjon@gmail.com>
2020-10-22 00:39:07 -0600

    initial commit of the directory tool. This will be the general use cases of this script

---
+++ analyze_directory.py
@@ -0,0 +1,31 @@
+import json
+import argparse
+
+
+#============================= DEFINE ARGPARSE =============================
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--new", help="Creates a new configuration to fill out, and execute.")
+ap.add_argument("-e", "--execute", help="Executes configuration file.")
+ap.add_argument("-v", "--verbose", help="This argument turns debugging prints on so you can see more information about the utility running.")
+args = vars(ap.parse_args())
+
+
+#============================= MAIN - Either Create a file or Execute a Config =============================
+
+
+def analyzeArgs(args):
+    if args["verbose"] is not None:
+        print("Turning verbose on..")
+    if args["new"] is not None:
+        createConfig(args["new"])
+        return
+    if args["execute"] is not None:
+        print(f"Executing {execute}")
+    else:
+        print("Error: Create a file, or execute one already created.")
+
+def createConfig(fileName):
+    print(f"Creating Config File... {fileName}")
+
+analyzeArgs(args)
