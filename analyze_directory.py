ef0e1fe3ad5e99fb53c3d6ac64cc179e98ca3520
Edison Earnest <dayxjon@gmail.com>
2020-11-06 01:55:13 -0700

    seperating global variables, and adding some additional configurations and default values

--- analyze_directory.py
+++ analyze_directory.py
@@ -10,6 +10,17 @@ ap.add_argument("-e", "--execute", help="Executes configuration file.")
 ap.add_argument("-v", "--verbose", help="This argument turns debugging prints on so you can see more information about the utility running.")
 args = vars(ap.parse_args())
 
+#============================= DEFINE GLOBALS =============================
+VERBOSE = False
+DEFAULT_JSON_WRITE_INDENT = 4
+DEFAULT_DIRECTORY_TEMPLATE = {
+    "os": "[windows/linux]",
+    "directory": "[directory/path]",
+    "type": "[local|remote]",
+    "username": "<only remote - not needed for local usage>",
+    "[password|key]": "<only remote - not needed for local usage>",
+    "filesize": "[b|kb|gb]"
+}
 
 #============================= MAIN - Either Create a file or Execute a Config =============================
 
@@ -17,6 +28,7 @@ args = vars(ap.parse_args())
 def analyzeArgs(args):
     if args["verbose"] is not None:
         print("Turning verbose on..")
+        VERBOSE = True
     if args["new"] is not None:
         createConfig(args["new"])
         return
@@ -26,19 +38,13 @@ def analyzeArgs(args):
         print("Error: Create a file, or execute one already created.")
 
 def createConfig(fileName):
-    print(f"Creating Config File... {fileName}")
-    
-    init_directory_template = [{
-        "directory": "",
-        "type": "[local|remote]",
-        "username": "<only remote - not needed for local usage>",
-        "password": "<only remote - not needed for local usage>",
-        "filesize": "[b|kb|gb]"
-    }]
+    init_directory_template = [DEFAULT_DIRECTORY_TEMPLATE]
 
     # convert into JSON:
+    if VERBOSE:
+        print(f"Writing Default Config to file '{fileName}'")
     f = open(fileName, "w")
-    f.write(json.dumps(init_directory_template, indent=4))
+    f.write(json.dumps(init_directory_template, indent=DEFAULT_JSON_WRITE_INDENT))
     f.close()
 
 
